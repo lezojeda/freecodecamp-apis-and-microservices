5a30cfea445b70e6fdee0e296ae36a2555eb1a62
Lucas <lezojeda@gmail.com>
2019-09-06 11:43:56 -0300

    URL shortener route handler first test, all ok

--- apis-and-microservices/url-shortener.js
+++ apis-and-microservices/url-shortener.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const bodyParser = require('body-parser')
 
+router.use(bodyParser.urlencoded({extended: false}))
+
+router.post('/api/shorturl/new', (req, res) => {
+    res.send("Hola amigos")
+})
 
 module.exports = router;
