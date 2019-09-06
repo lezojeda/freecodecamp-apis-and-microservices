f58c59e16d8a775caac54ab3dd2eadc92868572e
Lucas <lezojeda@gmail.com>
2019-09-06 12:58:43 -0300

    URL validation

--- apis-and-microservices/url-shortener.js
+++ apis-and-microservices/url-shortener.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const router = express.Router()
 const bodyParser = require('body-parser')
+const dns = require('dns');
 
 router.use(bodyParser.urlencoded({extended: false}))
 
 router.use(bodyParser.json())
 
 router.post('/api/shorturl/new', (req, res) => {
-    console.log(req.body)
-    res.json( {longurl : req.body})
+    const remove_https = /^https?:\/\//i
+    const longurl = req.body.longurl.replace(remove_https, '')
+    dns.lookup(longurl, (err) => {
+            if(err) {
+                res.json({"error" : "invalid URL"})
+            }
+        })
+    res.json({"Your requested URL" : req.body.longurl})
 })
 
 module.exports = router;
