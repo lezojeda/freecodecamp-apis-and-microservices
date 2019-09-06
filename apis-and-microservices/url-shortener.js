85d43da5fec20f024030572d2c915c6096086bb5
Lucas <lezojeda@gmail.com>
2019-09-06 12:17:23 -0300

    Getting input text from body just fine

--- apis-and-microservices/url-shortener.js
+++ apis-and-microservices/url-shortener.js
@@ -4,8 +4,11 @@ const bodyParser = require('body-parser')
 
 router.use(bodyParser.urlencoded({extended: false}))
 
+router.use(bodyParser.json())
+
 router.post('/api/shorturl/new', (req, res) => {
-    res.send("Hola amigos")
+    console.log(req.body)
+    res.json( {longurl : req.body})
 })
 
 module.exports = router;
