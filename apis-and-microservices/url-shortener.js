c59d81984021f484793ec70db98aca8aec429a73
Lucas <lezojeda@gmail.com>
2019-09-06 15:23:06 -0300

    trailling dash removal for long url request

--- apis-and-microservices/url-shortener.js
+++ apis-and-microservices/url-shortener.js
@@ -3,19 +3,26 @@ const router = express.Router()
 const bodyParser = require('body-parser')
 const dns = require('dns');
 
-router.use(bodyParser.urlencoded({extended: false}))
+router.use(bodyParser.urlencoded({ extended: false }))
 
 router.use(bodyParser.json())
 
 router.post('/api/shorturl/new', (req, res) => {
-    const remove_https = /^https?:\/\//i
-    const longurl = req.body.longurl.replace(remove_https, '')
+    const remove_https = /(^http(s)?:\/\/)/i
+    const remove_final_dash = /(\/)$/
+    const longurl = "www." + req.body.longurl.replace(remove_https, '').replace(remove_final_dash, '')
+    
     dns.lookup(longurl, (err) => {
-            if(err) {
-                res.json({"error" : "invalid URL"})
-            }
-        })
-    res.json({"Your requested URL" : req.body.longurl})
+        if (err) {
+            return res.json({ "error": "invalid URL" })
+        } else {
+            res.json({
+                "long_url (requested)": req.body.longurl,
+                "short_url": req.body.longurl
+            })
+            return;
+        }
+    })
 })
 
 module.exports = router;
