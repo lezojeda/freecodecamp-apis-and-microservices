e6616b0546a5131528428f4c28801293714308d8
Lucas <lezojeda@gmail.com>
2019-09-09 18:28:50 -0300

    Removed unnecessary uppercase in username fields. Validation of username before adding exercises.

--- apis-and-microservices/exercise-tracker.js
+++ apis-and-microservices/exercise-tracker.js
@@ -7,18 +7,18 @@ const mongoose = require('mongoose')
 // Create New User
 
 let userSchema = mongoose.Schema({
-    userName: {
+    username: {
         type: String,
         required: true
     }
 })
 
-let User = mongoose.model('user', userSchema) // Compile schema into model
+let User = mongoose.model('user', userSchema, 'users') // Compile schema into model
 
 router.post('/api/exercise/new-user', (req, res) => {
     // create new user document
     let user = new User();
-    user.userName = req.body.username
+    user.username = req.body.username
 
     user.save((err) => {
         if (err) {
@@ -34,8 +34,57 @@ router.post('/api/exercise/new-user', (req, res) => {
 
 // Add exercises
 
+let exerciseSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
+    duration: {
+        type: Number,
+        required: true
+    },
+
+    date: {
+        type: Date,
+        required: false
+    }
+})
+
+let Exercise = mongoose.model('exercise', exerciseSchema, 'exercises')
+
 router.post('/api/exercise/add', (req, res) => {
-    res.send("hi there")
+    User.findOne({ username: req.body.username }, (err, doc) => {
+        if (err) {
+            console.log("error at findOne")
+            console.log("err")
+            return;
+        } else if (doc == null) {
+            console.log("Username validation error")
+            res.send("No such username, please try again")
+        } else {
+            let exercise = new Exercise()
+            exercise.username = req.body.username
+            exercise.description = req.body.description
+            exercise.duration = req.body.duration
+            exercise.date = req.body.date
+
+            exercise.save((err) => {
+                if (err) {
+                    console.log(err)
+                    return;
+                } else {
+                    console.log('Exercise added')
+                    res.send("Exercise added")
+                }
+            })
+        }
+    })
 })
 
 module.exports = router;
