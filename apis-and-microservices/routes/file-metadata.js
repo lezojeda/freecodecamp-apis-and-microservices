6122ee0e25c97b199cac0e4834bf3049afcf57ed
Lucas <lezojeda@gmail.com>
2019-09-16 14:32:28 -0300

    File metadata route handler

--- apis-and-microservices/routes/file-metadata.js
+++ apis-and-microservices/routes/file-metadata.js
@@ -3,8 +3,10 @@ const router = express.Router()
 const multer = require('multer')
 let upload = multer({ dest: 'uploads/' })
 
-router.post('/api/fileanalyse', (req, res) => {
-
+router.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+    res.json({name: req.file.originalname, 
+              type: req.file.mimetype,
+              size: req.file.size + " bytes"})
 })
 
 module.exports = router
