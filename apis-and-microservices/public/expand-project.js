771a261f6493983f1718a8b3d718273d3700341e
Lucas <lezojeda@gmail.com>
2019-09-16 14:04:50 -0300

    Toggle visibility of projects in index.html script

--- apis-and-microservices/public/expand-project.js
+++ apis-and-microservices/public/expand-project.js
@@ -1,17 +1,17 @@
 let classname = document.getElementsByClassName('fa-chevron-circle-down') // Get all buttons with the arrow icon
 
 Array.from(classname).forEach(function(element) {
-    element.addEventListener('click', toggleVisibility);
+    element.addEventListener('click', toggleVisibilityAndRotate);
 }); //add event listener to all elements with the class of the icon
 
 
-function toggleVisibility(e) {
-    console.log(e.target.parentNode)
+function toggleVisibilityAndRotate(e) {
     let targetParentNode = e.target.parentNode
     if(targetParentNode.nextElementSibling.style.display == "none") {
         targetParentNode.nextElementSibling.style.display = "flex"
+        e.target.style.transform = "rotate(180deg)"
     } else {
         targetParentNode.nextElementSibling.style.display = "none"
+        e.target.style.transform = "rotate(0deg)"
     }
 }
-
